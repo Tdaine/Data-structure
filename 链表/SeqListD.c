eccffe12363aaea625b4ce0a6dc5fe8f67629f31
Tdaine <1916686509@qq.com>
2018-10-22 20:14:57 +0800

    动态顺序表

---
+++ 链表/SeqListD.c
@@ -0,0 +1,14 @@
+﻿#include"SeqListD.h"
+
+void InitSeqList(pSeqList pSeq)//初始化
+{
+	assert(pSeq != NULL);
+	pSeq->data = (DataType*)calloc(COUNT, sizeof(DataType));
+	if (pSeq->data == NULL)
+	{
+		perror("InitSeqList::use calloc");
+		exit(EXIT_FAILURE);
+	}
+	pSeq->sz = 0;
+	pSeq->capacity = COUNT;
+}
---
+++ 链表/SeqListD.h
@@ -0,0 +1,20 @@
+﻿#ifndef __SEQLISTD_H__
+#define __SEQLISTD_H__
+
+#include<stdio.h>
+#include<Windows.h>
+#include<stdlib.h>
+#include<assert.h>
+
+#define COUNT 6
+typedef int DataType;
+typedef struct SeqList
+{
+	DataType *data;
+	int sz;//现存元素个数
+	int capacity;//容量
+}SeqList,*pSeqList;
+void InitSeqList(pSeqList pSeq);
+
+
+#endif //__SEQLISTD_H__
