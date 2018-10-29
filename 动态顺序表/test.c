d78e24ed7713428d8e18018dfe754dc94f25c087
Tdaine <1916686509@qq.com>
2018-10-29 23:27:13 +0800

    动态顺序表

---
+++ 动态顺序表/test.c
@@ -0,0 +1,20 @@
+﻿
+#include"SeqListD.h"
+
+void test()
+{
+	SeqList seq;
+	int i = 0;
+	InitSeqList(&seq);
+	for(i=0;i<)
+	PushBack();
+}
+
+
+
+int main()
+{
+	test();
+	system("pause");
+	return 0;
+}
